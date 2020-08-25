ff68a2fb983a4601c9db6b18b8e73644415ae68e
tuantuan98 <dang.hoang.tuan@sun-asterisk.com>
2020-08-25 13:26:49 +0700

    create exam

--- gogoedu/urls.py
+++ gogoedu/urls.py
@@ -4,9 +4,11 @@ from . import views
 urlpatterns = [
 	path('', views.index, name='index'),
 	path('register/', views.register, name='register'),
-	path('profile/<int:pk>', views.profile, name='profile-detail'),
+	path('profile/<int:pk>', views.Profile.as_view(), name='profile-detail'),
 	path('lesson/<int:pk>', views.Lesson_detail.as_view(), name='lesson-detail'),
 	path('catagory/', views.CatagoryListView.as_view(), name='catagory'),
 	path('catagory/<int:pk>', views.CatagoryDetailView.as_view(), name='lesson'),
 	path('profile/<int:pk>/edit', views.profile_update, name='profile-update'),
+	path('test/<int:pk>', views.test_detail_view, name='test-detail'),
+	path('results/<int:pk>', views.show_results, name='show_results'),
 ]
